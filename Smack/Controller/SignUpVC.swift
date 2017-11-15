6610043010b2b397eecf6de548af5833bf1583f7
Mohamed SADAT <moh12594@gmail.com>
2017-11-15 23:09:18 +0100

    Sign Up added and Rounded button class View

--- Smack/Controller/SignUpVC.swift
+++ Smack/Controller/SignUpVC.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class SignUpVC: UIViewController {
 
+  // Outlets
+  @IBOutlet weak var userNameText: UITextField!
+  @IBOutlet weak var emailText: UITextField!
+  @IBOutlet weak var passwordText: UITextField!
+  
+  @IBOutlet weak var profileImage: UIImageView!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
 
@@ -19,5 +26,24 @@ class SignUpVC: UIViewController {
     performSegue(withIdentifier: UNWIND, sender: nil)
     UIApplication.shared.statusBarStyle = .lightContent
   }
+  
+  @IBAction func signUpButtonPressed(_ sender: Any) {
+    guard let email = emailText.text, emailText.text != "" else {return}
+    guard let password = passwordText.text, passwordText.text != "" else {return}
+    
+    AuthService.instance.registerUser(email: email, password: password) { (success) in
+      if success {
+        print("Registred user !")
+      }
+    }
+    
+  }
+  
+  @IBAction func pickAvatarPressed(_ sender: Any) {
+  }
+  
+  @IBAction func pickBGColorPressed(_ sender: Any) {
+  }
+  
   
 }
