836f30e335c77683d8109d1156aec9c3d07f58de
Mohamed SADAT <moh12594@gmail.com>
2017-11-15 22:43:48 +0100

    Add the AuthService and url api

--- Smack/Utilities/Constants.swift
+++ Smack/Utilities/Constants.swift
@@ -8,7 +8,19 @@
 
 import Foundation
 
+typealias CompletionHandler = (_ Success: Bool) -> ()
+
+// Api URL Constant
+let BASE_URL = "https://slackysmack.herokuapp.com/v1/"
+let URL_REGISTER = "\(BASE_URL)account/register"
+
+
 // Segues
 let TO_LOGIN = "toLogin"
 let TO_SIGNUP = "toSignUp"
 let UNWIND = "unwindToChannel"
+
+// User Defaults
+let TOKEN_KEY = "key"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
