215472b7760dd6cfbb52026a016fdd8d90dd4fd1
Mohamed SADAT <moh12594@gmail.com>
2017-11-20 10:25:21 +0100

    Add user creation

--- Smack/Controller/SignUpVC.swift
+++ Smack/Controller/SignUpVC.swift
@@ -17,6 +17,10 @@ class SignUpVC: UIViewController {
   
   @IBOutlet weak var profileImage: UIImageView!
   
+  // Variables
+  var avatarName = "profileDefault"
+  var avatarColor = "[0.5, 0.5, 0.5, 1]"
+  
   override func viewDidLoad() {
     super.viewDidLoad()
 
@@ -28,6 +32,7 @@ class SignUpVC: UIViewController {
   }
   
   @IBAction func signUpButtonPressed(_ sender: Any) {
+    guard let name = userNameText.text, userNameText.text != "" else {return}
     guard let email = emailText.text, emailText.text != "" else {return}
     guard let password = passwordText.text, passwordText.text != "" else {return}
     
@@ -35,7 +40,12 @@ class SignUpVC: UIViewController {
       if success {
         AuthService.instance.loginUser(email: email, password: password, completion: { (success) in
           if success {
-            print("Logged in user !", AuthService.instance.authToken)
+            AuthService.instance.createUser(name: name, email: email, avatarName: self.avatarName, avatarColor: self.avatarColor, completion: { (success) in
+              if success {
+                print(UserDataService.instance.name, UserDataService.instance.avatarName)
+                self.performSegue(withIdentifier: UNWIND, sender: nil)
+              }
+            })
           }
         })
       }
